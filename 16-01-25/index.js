3aba4354db1a03383a40f4cd39875e304fb34af0
Jainil2 <chauhanjainil305@gmail.com>
2025-01-16 17:28:23 +0530

    try upper_case npm module to practice npm module use

--- 16-01-25/index.js
+++ 16-01-25/index.js
@@ -1,17 +1,24 @@
 import http from 'http';
 import fs from 'fs';
 import url from 'url';
+import uc from 'upper-case';
 
-http.createServer((req, res) => {
-    let q = url.parse(req.url, true);
-    let filename = "." + q.pathname;
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
+// http.createServer((req, res) => {
+//     let q = url.parse(req.url, true);
+//     let filename = "." + q.pathname;
+//     fs.readFile(filename, function (err, data) {
+//         if (err) {
+//             res.writeHead(404, { 'Content-Type': 'text/html' });
+//             return res.end("404 Not Found");
+//         }
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write(data);
+//         return res.end();
+//     });
+// }).listen(8080);
+
+http.createServer((req, res) => {  
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(uc.upperCase("Hello World!"));
+    res.end();
 }).listen(8080);
