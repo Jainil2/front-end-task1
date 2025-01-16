58548dffac0c313d6c9245dd5cc74b46ecae8fbb
Jainil2 <chauhanjainil305@gmail.com>
2025-01-16 18:15:07 +0530

    complete functionality to send mail using nodemailer

--- 16-01-25/index.js
+++ 16-01-25/index.js
@@ -4,6 +4,9 @@ import url from 'url';
 import uc from 'upper-case';
 import event from 'events';
 import * as formidable from 'formidable';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // use of url, fs, and http modules
 // http.createServer((req, res) => {
@@ -39,42 +42,72 @@ import * as formidable from 'formidable';
 // eventEmitter.emit('scream');
 //---------------------------------------------------------
 //upload file using formidable module
-http.createServer(function (req, res) {
-    if (req.url == '/fileupload' && req.method.toLowerCase() === 'post') {
-        var form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, files) {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Error parsing the form');
-                return;
-            }
-            console.log(files);
-            var file = files.filetoupload[0]; // Access the first element of the array
-            var oldpath = file.filepath || file.path;
-            var newpath = 'C:\\Users\\chauh\\OneDrive\\Desktop\\' + file.originalFilename;
-            fs.rename(oldpath, newpath, function (err) {
-                if (err) {
-                    res.writeHead(500, { 'Content-Type': 'text/plain' });
-                    res.end('Error moving the file');
-                    return;
-                }
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end('File uploaded and moved!');
-            });
-        });
-    } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(`
-            <form action="/fileupload" method="post" enctype="multipart/form-data">
-                <input type="file" name="filetoupload"><br>
-                <input type="submit">
-            </form>
-        `);
-    }
-}).listen(3000, (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Server is running on port 3000');
-    }
-});
+// http.createServer(function (req, res) {
+//     if (req.url == '/fileupload' && req.method.toLowerCase() === 'post') {
+//         var form = new formidable.IncomingForm();
+//         form.parse(req, function (err, fields, files) {
+//             if (err) {
+//                 res.writeHead(500, { 'Content-Type': 'text/plain' });
+//                 res.end('Error parsing the form');
+//                 return;
+//             }
+//             console.log(files);
+//             var file = files.filetoupload[0]; 
+//             var oldpath = file.filepath || file.path;
+//             var newpath = 'C:\\Users\\chauh\\OneDrive\\Desktop\\' + file.originalFilename;
+//             fs.rename(oldpath, newpath, function (err) {
+//                 if (err) {
+//                     res.writeHead(500, { 'Content-Type': 'text/plain' });
+//                     res.end('Error moving the file');
+//                     return;
+//                 }
+//                 res.writeHead(200, { 'Content-Type': 'text/plain' });
+//                 res.end('File uploaded and moved!');
+//             });
+//         });
+//     } else {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.end(`
+//             <form action="/fileupload" method="post" enctype="multipart/form-data">
+//                 <input type="file" name="filetoupload"><br>
+//                 <input type="submit">
+//             </form>
+//         `);
+//     }
+// }).listen(3000, (err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log('Server is running on port 3000');
+//     }
+// });
+//---------------------------------------------------------
+//use of nodemailer module
+// if (!process.env.PASS) {
+//     console.error('Error: PASS environment variable is not set.');
+//     process.exit(1);
+// }
+
+// var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'jainilc94@gmail.com', 
+//         pass: process.env.PASS
+//     }
+// });
+
+// var mailOptions = {
+//     from: 'jainilc94@gmail.com',
+//     to: 'chauhanjainil305@gmail.com',
+//     subject: 'Sending Email using Node.js',
+//     html: '<h1>Welcome</h1><p>That was easy!</p>'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email sent: ' + info.response);
+//     }
+// });
+  
