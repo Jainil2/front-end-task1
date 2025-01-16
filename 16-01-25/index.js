a4b37aa49b4ef5c66189870804036a09e7de2885
Jainil2 <chauhanjainil305@gmail.com>
2025-01-16 17:35:07 +0530

    practice event emitter in node.js

--- 16-01-25/index.js
+++ 16-01-25/index.js
@@ -2,7 +2,9 @@ import http from 'http';
 import fs from 'fs';
 import url from 'url';
 import uc from 'upper-case';
+import event from 'events';
 
+// use of url, fs, and http modules
 // http.createServer((req, res) => {
 //     let q = url.parse(req.url, true);
 //     let filename = "." + q.pathname;
@@ -16,9 +18,22 @@ import uc from 'upper-case';
 //         return res.end();
 //     });
 // }).listen(8080);
+//---------------------------------------------------------
+//use of npm upper-case module
+// http.createServer((req, res) => {  
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write(uc.upperCase("Hello World!"));
+//     res.end();
+// }).listen(8080);
+//---------------------------------------------------------
+//use of npm events module
+// let eventEmitter = new event.EventEmitter();
 
-http.createServer((req, res) => {  
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(uc.upperCase("Hello World!"));
-    res.end();
-}).listen(8080);
+// var myEventHandler = function () {
+//     console.log('I hear a scream!');
+// }
+
+// eventEmitter.on('scream', myEventHandler);
+
+// eventEmitter.emit('scream');
+//---------------------------------------------------------
